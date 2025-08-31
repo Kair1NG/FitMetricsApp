a1df6d75dba36b8589bc18fa4749589e2b716992
Hans Ocampo <animeisthebesthaha@gmail.com>
2025-08-31 18:47:34 +0800

    Add Centralized styling process

--- fit_metrics/lib/screens/logIn.dart
+++ fit_metrics/lib/screens/logIn.dart
@@ -1,11 +1,12 @@
-import 'package:fit_metrics/common/widgets/appBar.dart';
-import 'register.dart';
+import 'package:fit_metrics/common/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 
 class LogIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //no database yet lmao
+      //cant register nor log in
       appBar: CommonAppBar(icon: Icons.login, title: 'Log In'),
       body: Center(child: Text('Log In Screen')),
     );
